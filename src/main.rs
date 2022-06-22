91e11af0422e206b008266fe3e26030c20d06776
impossible98 <christoleavenow@outlook.com>
2022-06-22 23:01:26 +0800

    feat: init project

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 mod global;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use local_ip_address::local_ip;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -18,16 +19,29 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn run(ip: &str, port: u16) -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind((ip, port))?
+    .run()
+    .await
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
 
-    let mode = env::var("MODE").unwrap_or("development".to_string());
+    let mode = env::var("MODE").unwrap_or("production".to_string());
     let port: u16 = env::var("PORT")
         .unwrap_or("6969".to_string())
         .parse()
         .unwrap();
 
+    let local_ip = local_ip().unwrap();
     println!();
     println!(
         "  {} v2.9.12 dev server running at:",
@@ -35,30 +49,14 @@ async fn main() -> std::io::Result<()> {
     );
     println!();
     println!("  > Local:    http://localhost:{}/", port);
-    println!("  > Network:  http://192.168.1.192:{}/", port);
+    println!("  > Network:  http://{}:{}/", local_ip, port);
     println!();
     println!("  ready in 473ms.");
     println!();
 
-    if mode == "production" {
-        HttpServer::new(|| {
-            App::new()
-                .service(hello)
-                .service(echo)
-                .route("/hey", web::get().to(manual_hello))
-        })
-        .bind(("127.0.0.1", port))?
-        .run()
-        .await
+    if mode == "development" {
+        run("0.0.0.0", port).await
     } else {
-        HttpServer::new(|| {
-            App::new()
-                .service(hello)
-                .service(echo)
-                .route("/hey", web::get().to(manual_hello))
-        })
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+        run("127.0.0.1", port).await
     }
 }
